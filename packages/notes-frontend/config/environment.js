2402304dc6eff916f5fffd5c0f3fae8cf1906ba8
Thomas <thomaswcuster@gmail.com>
2019-10-24 15:55:56 -0500

    Ember 3.13

--- packages/notes-frontend/config/environment.js
+++ packages/notes-frontend/config/environment.js
@@ -24,15 +24,15 @@ module.exports = function(environment) {
     };
 
     if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+        ENV.APP.LOG_RESOLVER = true;
+        ENV.APP.LOG_ACTIVE_GENERATION = true;
+        ENV.APP.LOG_TRANSITIONS = true;
+        ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        ENV.APP.LOG_VIEW_LOOKUPS = true;
     }
 
     if (environment === 'test') {
-    // Testem prefers this...
+        // Testem prefers this...
         ENV.locationType = 'none';
 
         // keep test console output quieter
@@ -44,7 +44,7 @@ module.exports = function(environment) {
     }
 
     if (environment === 'production') {
-    // here you can enable a production-specific feature
+        // here you can enable a production-specific feature
     }
 
     return ENV;
