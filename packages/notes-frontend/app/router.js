db02c7d2492a04be14051666c793d01e80e8736d
Thomas <thomaswcuster@gmail.com>
2020-11-10 15:38:26 -0600

    Login page

--- packages/notes-frontend/app/router.js
+++ packages/notes-frontend/app/router.js
@@ -7,21 +7,22 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-    this.route('notes', function() {
-        this.route('new');
-        this.route('edit', { path: '/:note_id' });
-    });
-    this.route('collections', function() {
-        this.route('new');
-        this.route('edit', { path: '/:collection_id' });
-    });
-    this.route('collection-notes', { path: '/collections/:collection_id/notes' }, function() {
-        this.route('new');
-        this.route('edit', { path: '/:note_id' });
-    });
-    this.route('tags', function() {
-        this.route('new');
-        this.route('edit', { path: '/:tag_id' });
-    });
-    this.route('trash');
+  this.route('notes', function() {
+      this.route('new');
+      this.route('edit', { path: '/:note_id' });
+  });
+  this.route('collections', function() {
+      this.route('new');
+      this.route('edit', { path: '/:collection_id' });
+  });
+  this.route('collection-notes', { path: '/collections/:collection_id/notes' }, function() {
+      this.route('new');
+      this.route('edit', { path: '/:note_id' });
+  });
+  this.route('tags', function() {
+      this.route('new');
+      this.route('edit', { path: '/:tag_id' });
+  });
+  this.route('trash');
+  this.route('login');
 });
