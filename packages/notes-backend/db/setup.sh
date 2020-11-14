f4c7e257aee2c99b442ad2945aa5b2dcce618535
Thomas <thomaswcuster@gmail.com>
2020-11-14 16:01:59 -0600

    DB credentials from env vars

--- packages/notes-backend/db/setup.sh
+++ packages/notes-backend/db/setup.sh
@@ -2,11 +2,13 @@
 
 echo "Setting up Postgres user and database..."
 
+source .env
+
 sudo -iu postgres bash << END
-psql -c "CREATE ROLE notes_owner WITH CREATEDB CREATEROLE LOGIN PASSWORD 'seton-owner';"
-psql -c "CREATE ROLE notes_user WITH LOGIN PASSWORD 'seton-user';"
-psql -c "GRANT notes_user TO notes_owner;"
-createdb notes -O notes_owner
+psql -c "CREATE ROLE $OWNER_NAME WITH CREATEDB CREATEROLE LOGIN PASSWORD '$OWNER_PW';"
+psql -c "CREATE ROLE $USER_NAME WITH LOGIN PASSWORD '$USER_PW';"
+psql -c "GRANT $USER_NAME TO $OWNER_NAME;"
+createdb notes -O $OWNER_NAME
 psql -d "notes" -c "DROP SCHEMA IF EXISTS public;"
 END
 
