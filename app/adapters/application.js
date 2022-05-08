6d67c61f3258a8fb6aa4f37ed2a1027ac5d5d3e8
Thomas Custer <thomaswcuster@gmail.com>
2022-05-07 20:32:09 -0500

    Fixing ember-simple-auth deprecation

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,25 @@
 import DS from 'ember-data';
-import TokenAdapterMixin from 'ember-simple-auth-token/mixins/token-adapter';
+import { inject } from '@ember/service';
 
-export default DS.JSONAPIAdapter.extend(TokenAdapterMixin, {
-    namespace: 'api'
+// Attempting to migrate this to a native class broke everything.
+export default DS.JSONAPIAdapter.extend({
+    session: inject('session'),
+    namespace: 'api',
+
+    get headers() {
+        if (this.session.isAuthenticated) {
+            return {
+                Authorization: `Bearer ${this.session.data.authenticated.token}`,
+            };
+        } else {
+            return {};
+        }
+    },
+
+    handleResponse(status) {
+        if (status === 401 && this.session.isAuthenticated) {
+            this.session.invalidate();
+        }
+        return this._super(...arguments);
+    },
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-    beforeModel() {
+    @service session;
+
+    async beforeModel() {
+        await this.session.setup();
         this.transitionTo('notes');
     }
 }
