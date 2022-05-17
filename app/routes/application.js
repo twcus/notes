956fa12485968d513c6904a9d99619c987cad4a4
Thomas Custer <thomaswcuster@gmail.com>
2022-05-16 21:49:53 -0500

    Fixing ember-simple-auth warning, removing debug console output, fixing password input type on login form

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class IndexRoute extends Route {
+export default class Application extends Route {
     @service session;
-    @service router;
 
     async beforeModel() {
-        this.router.transitionTo('notes');
+        await this.session.setup();
     }
 }
